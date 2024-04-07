kemaleren/fiftyone

persist space sizes in session

app/packages/spaces/src/SpaceNode.tsx
@@ -11,6 +11,7 @@ export default class SpaceNode {
   // if layout is set, render as space container instead of panel container
   layout?: Layout;
   pinned?: boolean;
+  sizes?: number[];
   constructor(
     id?: string,
     children?: SpaceNode[],
@@ -109,6 +110,7 @@ export default class SpaceNode {
       layout: this.layout,
       pinned: this.pinned,
       activeChild: this.activeChild,
+      sizes: this.sizes,
     };
   }
 }

app/packages/spaces/src/SpaceTree.ts
@@ -62,6 +62,7 @@ export default class SpaceTree {
     let ancestorNode = parentNode;
     if (parentNode?.parent && parentNode?.children.length === 1) {
       ancestorNode = parentNode?.parent;
+      ancestorNode.sizes = undefined;
       parentNode?.remove();
       this.joinNode(ancestorNode);
     } else if (ancestorNode) {
@@ -79,6 +80,11 @@ export default class SpaceTree {
       node.parent.activeChild = node.id;
       this.updateTree(node);
     }
+  }
+
+  setNodeSizes(node: SpaceNode, sizes: number[]) {
+    node.sizes = sizes;
+    this.updateTree(node);
   }
 
   // a method for moving a node in the tree

app/packages/spaces/src/types.ts
@@ -40,6 +40,7 @@ export type SpaceNodeJSON = {
   layout?: SpaceNode["layout"];
   type?: SpaceNode["type"];
   pinned?: SpaceNode["pinned"];
+  sizes?: number[];
 };
 
 export type PanelProps = {

fiftyone/core/spaces.py
@@ -5,6 +5,7 @@ App Space configuration.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
+
 from bson import ObjectId
 from mongoengine.errors import ValidationError
 import uuid
@@ -57,7 +58,8 @@ class Space(Component):
         orientation (["horizontal", "vertical"]): the orientation of this
             space's children
         active_child: the ``component_id`` of this space's currently active
-            chilld
+            child
+        sizes: the ordered list of sizes for children of space
     """
 
     meta = {"strict": False, "allow_inheritance": True}
@@ -70,6 +72,7 @@ class Space(Component):
         choices=["horizontal", "vertical"], default=None
     )
     active_child = fof.StringField(default=None)
+    sizes = fof.ListField(fof.FloatField(), default=None)
 
 
 samples_panel = Panel(type="Samples", pinned=True)

findtopher/fiftyone-archive

app/packages/spaces/src/SpaceNode.tsx
@@ -11,6 +11,7 @@ export default class SpaceNode {
   // if layout is set, render as space container instead of panel container
   layout?: Layout;
   pinned?: boolean;
+  sizes?: string[];
   constructor(
     id?: string,
     children?: SpaceNode[],
@@ -109,6 +110,7 @@ export default class SpaceNode {
       layout: this.layout,
       pinned: this.pinned,
       activeChild: this.activeChild,
+      sizes: this.sizes,
     };
   }
 }

app/packages/spaces/src/SpaceTree.ts
@@ -62,6 +62,7 @@ export default class SpaceTree {
     let ancestorNode = parentNode;
     if (parentNode?.parent && parentNode?.children.length === 1) {
       ancestorNode = parentNode?.parent;
+      ancestorNode.sizes = undefined;
       parentNode?.remove();
       this.joinNode(ancestorNode);
     } else if (ancestorNode) {
@@ -79,6 +80,11 @@ export default class SpaceTree {
       node.parent.activeChild = node.id;
       this.updateTree(node);
     }
+  }
+
+  setNodeSizes(node: SpaceNode, sizes: string[]) {
+    node.sizes = sizes;
+    this.updateTree(node);
   }
 
   // a method for moving a node in the tree

app/packages/spaces/src/types.ts
@@ -40,6 +40,7 @@ export type SpaceNodeJSON = {
   layout?: SpaceNode["layout"];
   type?: SpaceNode["type"];
   pinned?: SpaceNode["pinned"];
+  sizes?: string[];
 };
 
 export type PanelProps = {

fiftyone/core/spaces.py
@@ -5,6 +5,7 @@ App Space configuration.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
+
 from bson import ObjectId
 from mongoengine.errors import ValidationError
 import uuid
@@ -57,7 +58,8 @@ class Space(Component):
         orientation (["horizontal", "vertical"]): the orientation of this
             space's children
         active_child: the ``component_id`` of this space's currently active
-            chilld
+            child
+        sizes: the ordered list of sizes for children of space
     """
 
     meta = {"strict": False, "allow_inheritance": True}
@@ -70,6 +72,7 @@ class Space(Component):
         choices=["horizontal", "vertical"], default=None
     )
     active_child = fof.StringField(default=None)
+    sizes = fof.ListField(fof.StringField(), default=None)
 
 
 samples_panel = Panel(type="Samples", pinned=True)